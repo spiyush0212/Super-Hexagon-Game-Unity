8097d7817006ceeb75765eb5ec3442c7c4f7e6e6
Piyush Sharma <spiyush.0212@gmail.com>
2020-12-24 04:18:29 +0530

    Final working game code

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+    float movement = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * moveSpeed * -1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        ScoreScreen.scoreValue += 1;
+    }
+}
